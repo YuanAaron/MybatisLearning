e50677be28a1afef7ab5385a93888aaad682b5f9
yuanwei <yuanwei@kexinjinke.com>
2021-01-15 17:54:36 +0800

    Mybatis入门

--- mybatis/src/main/java/cn/coderap/test/MybatisTest.java
+++ mybatis/src/main/java/cn/coderap/test/MybatisTest.java
@@ -26,4 +26,40 @@ public class MybatisTest {
         System.out.println(objects);
         sqlSession.close();
     }
+
+    @Test
+    public void test2() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream); //用到了建造者模式
+        SqlSession sqlSession = sqlSessionFactory.openSession(); //用到了工厂模式
+        User user = new User();
+        user.setName("wangwu");
+        user.setAge(25);
+        sqlSession.insert("user.saveUser",user);
+        sqlSession.commit(); //增删改需要提交事务
+        sqlSession.close();
+    }
+
+    @Test
+    public void test3() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream); //用到了建造者模式
+        SqlSession sqlSession = sqlSessionFactory.openSession(); //用到了工厂模式
+        User user = new User();
+        user.setName("wangwu");
+        user.setAge(26);
+        sqlSession.update("user.updateUser",user);
+        sqlSession.commit(); //增删改需要提交事务
+        sqlSession.close();
+    }
+
+    @Test
+    public void test4() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream); //用到了建造者模式
+        SqlSession sqlSession = sqlSessionFactory.openSession(); //用到了工厂模式
+        sqlSession.delete("user.deleteUser","wangwu");
+        sqlSession.commit(); //增删改需要提交事务
+        sqlSession.close();
+    }
 }
